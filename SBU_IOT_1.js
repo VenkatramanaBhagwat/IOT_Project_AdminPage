21893795e5750c32621e4a25e15c1627d81b2f1a
Venkatramana Ramachandra Bhagwat <31812876+VenkatramanaBhagwat@users.noreply.github.com>
2017-11-26 22:08:21 -0500

    Add files via upload

---
+++ SBU_IOT_1.js
@@ -0,0 +1,40 @@
+console.log("ggg");
+
+
+function configureDropDownLists(ddl1,ddl2) {
+    var colours = ['','Main Hall', 'Room 101', 'Room 102'];
+    var shapes = ['','Hall', 'Ballroom A', 'Ballroom B'];
+    var names = ['','Room s10', 'Room s11', 'Room s12'];
+
+    switch (ddl1.value) {
+        case 'Colours':
+            ddl2.options.length = 0;
+            for (i = 0; i < colours.length; i++) {
+                createOption(ddl2, colours[i], colours[i]);
+            }
+            break;
+        case 'Shapes':
+            ddl2.options.length = 0;
+        for (i = 0; i < shapes.length; i++) {
+            createOption(ddl2, shapes[i], shapes[i]);
+            }
+            break;
+        case 'Names':
+            ddl2.options.length = 0;
+            for (i = 0; i < names.length; i++) {
+                createOption(ddl2, names[i], names[i]);
+            }
+            break;
+            default:
+                ddl2.options.length = 0;
+            break;
+    }
+
+}
+
+    function createOption(ddl, text, value) {
+        var opt = document.createElement('option');
+        opt.value = value;
+        opt.text = text;
+        ddl.options.add(opt);
+    }
